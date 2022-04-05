7e75fbb3e08e3f53107c31bd594e5be5b120021a
ds <19520216@gm.uit.edu.vn>
2022-04-05 21:11:12 +0700

    test

---
+++ app/src/main/java/com/example/todo/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide(); // hide the title bar
+
+    }
+}
