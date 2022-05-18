ebd714a9b63ea9f2087c86d2f915d293abee4f95
ds <19520216@gm.uit.edu.vn>
2022-05-18 23:45:06 +0700

    Cập nhật giao diện

--- app/src/main/java/com/example/todo/view/LoginActivity.java
+++ app/src/main/java/com/example/todo/view/LoginActivity.java
@@ -2,16 +2,30 @@ package com.example.todo.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.todo.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView openRegisterActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+        openRegisterActivity = (TextView) findViewById(R.id.openRegisterActivity);
+        openRegisterActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
